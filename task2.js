56be5f969d0c8cbfeaa4a04752da595f99fe440b
aishamuratnazarova <58081603+aishamuratnazarova@users.noreply.github.com>
2020-04-22 14:58:49 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,12 +1,4 @@
-function sum (array) {
-    let sum = 0;
-    if (Array.isArray(array)) sum = array.reduce((sum, num) => (sum + num));
-
-    return (
-        !Array.isArray(this) ?
-        array.reduce((sum, num) => (sum + num)) :
-        this.reduce((sum, num) => (sum + num)) + sum
-    )
+function sum() {
+    return this.reduce((a, v) => a + v, 0);
 }
-
 console.log(sum.call([5, 5, 5, 5, 5, 5, 5, 5, 5]));
