e8b19dd27144102de69b0df4a46f57643f4d6be9
aishamuratnazarova <58081603+aishamuratnazarova@users.noreply.github.com>
2020-04-22 17:07:33 +0300

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,12 +1,8 @@
 function User(name) {
-	this.name = name;
-	this.getName = () => (this.name);
-
-	return {
-		getName: this.getName
-	}
+    this.name = name;
 }
-
+User.prototype.getName = function() {
+    return this.name;
+};
 const me = new User('Rex');
-
 console.log(me.getName()); // Rex
